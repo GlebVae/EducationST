b72d988befd48e72a6fb693f33c82b19df682593
Gleb <mohax_k20@mail.ru>
2015-06-01 18:01:41 +0300

    задача

---
+++ test/src/iftest.java
@@ -0,0 +1,11 @@
+public class iftest {
+    public static void main (String [] args){
+        int x = 2;
+        if (x == 3){
+            System.out.println("x должен ровняться 3");
+        }
+        else {
+            System.out.println("x не равен 3");
+        }
+    }
+}
