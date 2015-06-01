67e58c8e764dc22d3f70f164e01ccbad17cc3db1
Gleb <mohax_k20@mail.ru>
2015-06-01 14:25:31 +0300

    2ndtry

---
+++ Party.java
@@ -0,0 +1,15 @@
+important java.awt.*;
+important java.awt.event*;
+
+class Party {
+	public void buildinvite() {
+
+	Frame f = new Frame();
+	Label l = new Label("Вечеринка у Глеба");
+	Button b = new Button("Ваша ставка");
+	Button c = new Button("Сбросить");
+	Pannel p = new Pannel();
+	p.add(l);
+	} //Еще код
+
+}
