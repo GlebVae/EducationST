418af5d45ae310bb9289942c49c667d9e0ebe8e7
Gleb <mohax_k20@mail.ru>
2015-06-03 10:15:04 +0300

    Библиотека. описание сущностей

---
+++ Library/src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String fname;
+    public String getFname(){
+        return fname;
+    }
+    public void setFname (String f){
+        fname = f;
+    }
+
+    private String sname;
+    public String getSname(){
+        return sname;
+    }
+    public void setSname (String s){
+        sname = s;
+    }
+
+    private int age;
+    public int getAge(){
+        return age;
+    }
+    public void setAge (int a){
+        age = a;
+    }
+}
