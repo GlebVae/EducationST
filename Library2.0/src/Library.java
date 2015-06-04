872f7e8d3cb5764ae34c95c395e131c52921440c
Gleb <mohax_k20@mail.ru>
2015-06-04 17:08:42 +0300

    updated

--- Library2.0/src/Library.java
+++ Library2.0/src/Library.java
@@ -1,8 +1,16 @@
 public class Library {
 
+    public void bookList(){
+        Book[] booklist;
+        booklist = new Book[1];
+        booklist[0] = new Book();
+    }
+    public void userCard(int idcard, String namecard, String surnamecard, int agecard ){
+        UserCard[] usercard;
+        usercard = new UserCard[1];
+        usercard [1] = new UserCard(1,"Артём","Лопатин",25);
+    }
 
-    UserCard[] usercards;
-    usercards = new UserCard[5];
 
 
 }
