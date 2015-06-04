5e2c0ea967cc6f95656970e495f7717378f3a212
Gleb <mohax_k20@mail.ru>
2015-06-04 17:27:32 +0300

    updated

--- Library2.0/src/Library.java
+++ Library2.0/src/Library.java
@@ -4,11 +4,20 @@ public class Library {
         Book[] booklist;
         booklist = new Book[1];
         booklist[0] = new Book();
+        booklist[0].setID(1);
+        booklist[0].setBookAuthor("Лермонтов");
+        booklist[0].setBookeName("Герои нашего времени");
+        booklist[0].setGender("Роман");
+        booklist[0].setBookYear("1840");
     }
     public void userCard(int idcard, String namecard, String surnamecard, int agecard ){
         UserCard[] usercard;
         usercard = new UserCard[1];
-        usercard [0] = new UserCard(1,"Артём","Лопатин",25);
+        usercard [0] = new UserCard();
+        usercard[0].setIDCard(1);
+        usercard[0].setNameCard("Артём");
+        usercard[0].setSurnameCard("Лопатин");
+        usercard[0].setAgeCard(25);
     }
 
 
