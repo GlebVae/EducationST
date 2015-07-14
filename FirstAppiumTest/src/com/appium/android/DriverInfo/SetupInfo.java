9efedbc65e1d9f934b8ffdc36c0679417f2f0da3
Gleb <mohax_k20@mail.ru>
2015-07-14 18:37:01 +0300

    new

--- FirstAppiumTest/src/com/appium/android/DriverInfo/SetupInfo.java
+++ FirstAppiumTest/src/com/appium/android/DriverInfo/SetupInfo.java
@@ -1,50 +1,35 @@
 package com.appium.android.DriverInfo;
-import com.thoughtworks.selenium.webdriven.commands.IsElementPresent;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 
-import java.awt.*;
-import java.awt.Dimension;
-import java.awt.image.BufferedImage;
+
 import java.io.*;
 import java.net.URL;
-import java.nio.Buffer;
-import java.util.NoSuchElementException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
-import junit.framework.Assert;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.imageio.ImageIO;
 
 public class SetupInfo {
     protected AppiumDriver<AndroidElement> driver;
 
     public void writeLog(String text) {
         try {
-            File f = new File("D://results//Log.txt");
+            File f = new File("/results/Log.txt");
             File dir = f.getParentFile();
             if (!f.exists()) {
                 dir.mkdir();
                 f.createNewFile();
             }
-//            FileWriter fw = new FileWriter(f.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(new FileWriter(f.getAbsoluteFile(), true));
             bw.write(text);
             bw.close();
-
         }catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,32 +38,30 @@ public class SetupInfo {
     protected void printInfo (String text) throws IOException {
         System.out.println(text);
         writeLog(text + "\n");
-
     }
 
     protected void takeScreenshot(String name)
     {
         try {
             File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(scrFile, new File("D:\\results\\screenshots\\scr" + name +".png"));
+            FileUtils.copyFile(scrFile, new File("results/screenshots/scr" + name +".png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    protected void waitElemByName(String name) throws Exception{
-        WebDriverWait waiter = new WebDriverWait(driver, 20);
+    protected void waitElemByName(String name, int timeWait) throws Exception{
+        WebDriverWait waiter = new WebDriverWait(driver, timeWait);
         waiter.until(ExpectedConditions.presenceOfElementLocated(By.name(name)));
     }
-    protected void waitElemByClassName(String name){
-        WebDriverWait waiter = new WebDriverWait(driver, 20);
+    protected void waitElemByClassName(String name, int timeWait) throws Exception{
+        WebDriverWait waiter = new WebDriverWait(driver, timeWait);
         waiter.until(ExpectedConditions.presenceOfElementLocated(By.className(name)));
     }
-
-//    protected void getLoc (String name){
-//
-//        System.out.println("just clicked element with loc: " + name.getLocation() + " and name: " + name.getText());
-//    }
+    protected void waitElemById (String name, int timeWait) throws Exception{
+        WebDriverWait waiter = new WebDriverWait(driver, timeWait);
+        waiter.until(ExpectedConditions.presenceOfElementLocated(By.id(name)));
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -97,8 +80,6 @@ public class SetupInfo {
         capabilities.setCapability("appActivity", "com.compareking.zappy.ui.activity.UnauthorizedActivity"); //activi
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
-
-
 
     @After
     public void tearDown() throws Exception {
---
+++ FirstAppiumTest/src/com/appium/android/tests/RegisterNewUser.java
@@ -0,0 +1,96 @@
+package com.appium.android.tests;
+
+import com.appium.android.DriverInfo.SetupInfo;
+import io.appium.java_client.android.AndroidElement;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+
+// унаследовал от класса родителя, описывающий драйвер
+public class RegisterNewUser extends SetupInfo {
+    @Test
+    public void chooseRegEmail() throws Exception {
+        printInfo("Test chooseRegEmail has started");
+        //нашли кнопку регистрациии через емейл
+
+        WebElement email = driver.findElement(By.name("Email"));
+        assertEquals("Email", email.getText());
+        printInfo("just clicked element with loc: " + email.getLocation() + " and name: " + email.getText());
+        email.click();
+        takeScreenshot("Email");
+
+        //нашли поля ввода данных и ввели их
+
+        List<AndroidElement> textFieldsList = driver.findElementsByClassName("android.widget.EditText");
+        textFieldsList.get(0).sendKeys("gerald@izrivii.com");
+        printInfo("Entered email: " + textFieldsList.get(0).getText());
+        textFieldsList.get(1).sendKeys("Ciri  Rivia");
+        printInfo("Entered username: " + textFieldsList.get(1).getText());
+        textFieldsList.get(2).sendKeys("12345678adb");
+        takeScreenshot("Login");
+
+        //нашли кнопку подтверждения регистрации
+
+        WebElement register = driver.findElementByName("REGISTER");
+        assertEquals("REGISTER", register.getText());
+        printInfo("just clicked element with loc: " + register.getLocation() + " and name: " + register.getText());
+        register.click();
+
+        //повесили ожидание экрана
+
+        waitElemByName("Done", 20);
+        takeScreenshot("Done");
+        WebElement backIcon = driver.findElement(By.className("android.widget.ImageButton"));
+        printInfo("just clicked element with loc: " + backIcon.getLocation() + " and name: " + backIcon.getTagName());
+        backIcon.click();
+        takeScreenshot("Returned from app");
+        waitElemByName("REGISTER", 20);
+
+        //вернлись на главный экран и проверили появление кнопки log in
+
+        WebElement backToStart = driver.findElement(By.className("android.widget.ImageButton"));
+        printInfo("Pressed Back Button");
+        driver.navigate().back();
+//  данный элемент существует, но при нажатии не происходит никаких действий, однако область под элементом работает
+//        printInfo("just clicked element with loc: " + backToStart.getLocation() + "and name: " + backToStart.getTagName());
+//        backToStart.click();
+        waitElemByName("LOG IN", 20);
+
+        //проверяем можно ли войти новым пользователем
+
+        WebElement login = driver.findElement(By.name("LOG IN"));
+        printInfo("just clicked element with loc: " + login.getLocation() + " and name: " + login.getText());
+        login.click();
+        List<AndroidElement> textFiledList = driver.findElementsByClassName("android.widget.EditText");
+        textFieldsList.get(0).sendKeys("gerald@izrivii.com");
+        printInfo("Entered email: " + textFieldsList.get(0).getText());
+        textFieldsList.get(1).sendKeys("12345678adb");
+        takeScreenshot("ChekingNewUserData");
+        printInfo("just clicked element with loc: " + login.getLocation() + "and name: " + login.getText());
+        driver.findElementByName("LOG IN").click();
+        waitElemByName("Done", 20);
+    }
+
+    @Test
+    public void wrongEmailData()throws Exception {
+
+    }
+}
+
+
+
